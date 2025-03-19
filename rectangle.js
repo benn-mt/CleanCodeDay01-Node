da68880c1e89424211335089f048823817eed02b
Ben Noble <ben.noble@madetech.com>
2025-03-19 12:20:02 +0000

    Basic rectangle code

---
+++ rectangle.js
@@ -0,0 +1,16 @@
+class Rectangle{
+    constructor(length, width){
+        this._length = length;
+        this._width = width;
+    }
+
+    area(){
+        return this._length * this._width;
+    }
+
+    perimeter(){
+        return this._length * 2 + this._width * 2;
+    }
+}
+
+module.exports = {Rectangle}
---
+++ rectangle.test.js
@@ -0,0 +1,19 @@
+const {Rectangle} = require("./rectangle");
+
+describe("Rectangles...", () =>{
+    test("Simple area calculations", () => {
+        expect(new Rectangle(1,3).area()).toEqual(3);
+        expect(new Rectangle(2,4).area()).toEqual(8);
+        expect(new Rectangle(4,3).area()).toEqual(12);
+        expect(new Rectangle(12,0).area()).toEqual(0);
+        expect(new Rectangle(0,6).area()).toEqual(0);
+        expect(new Rectangle(0,0).area()).toEqual(0);
+    });
+
+    test("Can caluclate perimeter", () => {
+        expect(new Rectangle(0,0).perimeter()).toEqual(0);
+        expect(new Rectangle(5,0).perimeter()).toEqual(10);
+        expect(new Rectangle(0,4).perimeter()).toEqual(8);
+        expect(new Rectangle(3,7).perimeter()).toEqual(20);
+    });
+})
